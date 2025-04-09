eaf6e11329c580c8625f7249e23d05f57e72df89
Administrator <mail@gmail.com>
2025-04-09 16:50:24 +0300

    Refactored

--- frontend/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Home() {
-    return (
-        <div className="text-center mt-10">
-            <h1 className="text-3xl font-bold">Hello from Next.js + Tailwind!</h1>
-        </div>
-    )
-}
